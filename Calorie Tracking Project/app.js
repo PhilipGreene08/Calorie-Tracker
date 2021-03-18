fcfe7476ef1734efe9e76f660a66a01faeefe835
PhilipGreene08 <54162361+PhilipGreene08@users.noreply.github.com>
2021-03-17 20:42:37 -0500

    Add files via upload

--- Calorie Tracking Project/app.js
+++ Calorie Tracking Project/app.js
@@ -16,9 +16,9 @@ const ItemCtrl = (function () {
     //data structure / state
     const data = {
         items: [
-            { id: 0, name: 'steak dinner', calories: 1200 },
-            { id: 1, name: 'cookie', calories: 400 },
-            { id: 2, name: 'eggs', calories: 300 },
+            // { id: 0, name: 'steak dinner', calories: 1200 },
+            // { id: 1, name: 'cookie', calories: 400 },
+            // { id: 2, name: 'eggs', calories: 300 },
         ],
         currentItem: null,
         totalCalories: 0
@@ -28,6 +28,26 @@ const ItemCtrl = (function () {
     return {
         getItems: function () {
             return data.items
+        },
+        addItem: function (name, calories) {
+            let id
+            //create ID
+            if (data.items.length > 0) {
+                id = data.items[data.items.length - 1].id + 1
+            } else {
+                id = 0
+            }
+
+            //calories to number form string
+            calories = parseInt(calories) //change to number 
+
+            //create new Item
+            newItem = new Item(id, name, calories)
+
+            //push new item to data.items
+            data.items.push(newItem)
+
+            return newItem
         },
         logData: function () {
             return data
@@ -44,8 +64,8 @@ const UICtrl = (function () {
     const UISelectors = {
         itemList: '#item-list',
         addBtn: '.add-btn',
-        itemName: '#item-name',
-        itemCalories: '#item-calories'
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories'
     }
 
     // public
@@ -65,9 +85,28 @@ const UICtrl = (function () {
         },
         getItemInput: function () {
             return {
-                name:document.querySelector(UISelectors.itemName),
-                calories:document.querySelector(UISelectors.itemCalories)
+                name: document.querySelector(UISelectors.itemNameInput).value,
+                calories: document.querySelector(UISelectors.itemCaloriesInput).value
             }
+        },
+        addListItem: function (item) {
+            //show the list 
+            document.querySelector(UISelectors.itemList).style.display = 'block'
+            //create li element
+            const li = document.createElement('li')
+            li.className = 'collection-item'
+            li.id = `item-${item.id}`
+            li.innerHTML = `<strong>${item.name}:</strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content"><i class=" edit-item fa fa-pencil"></i></a>`
+            //insert item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+        },
+        clearInput: function () {
+            document.querySelector(UISelectors.itemNameInput).value = ''
+            document.querySelector(UISelectors.itemCaloriesInput).value = ''
+        },
+        hideList: function () {
+            document.querySelector(UISelectors.itemList).style.display = ''
         },
         getSelectors: function () {
             return UISelectors
@@ -83,15 +122,24 @@ const UICtrl = (function () {
 const App = (function (ItemCtrl, UICtrl) {
     const loadEventListeners = function () {
         const UISelectors = UICtrl.getSelectors()
-        
         //add item
         document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit)
     }
 
     //add item submit
-    const itemAddSubmit = function(e){
+    const itemAddSubmit = function (e) {
         //get form input from UI controller
-        const input = UIController.getItemInput()
+        const input = UICtrl.getItemInput()
+        //check for name and calorie input
+        if (input.name !== '' && input.calories !== '') {
+            //add item
+            const newItem = ItemCtrl.addItem(input.name, input.calories)
+            //add to UI list
+            UICtrl.addListItem(newItem)
+            //clear input
+            UICtrl.clearInput()
+
+        }
 
         e.preventDefault()
     }
@@ -102,9 +150,13 @@ const App = (function (ItemCtrl, UICtrl) {
             //fetch items from data controller
             const items = ItemCtrl.getItems()
 
-            //populate list with items
-            UICtrl.populateItemList(items)
-            console.log(items)
+            //check if items exist
+            if (items.length === 0) {
+                UICtrl.hideList()
+            } else { //populate list with items
+                UICtrl.populateItemList(items)
+                console.log(items)
+            }
 
             //loadevent listeners
             loadEventListeners()
